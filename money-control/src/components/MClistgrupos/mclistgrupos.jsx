4ff2f8b4ae7c2cd4a3f3dadf0afe0f2fe882bb4e
vicnx <andanivicente@gmail.com>
2022-01-24 11:00:57 +0100

    New list groups css

--- money-control/src/components/MClistgrupos/mclistgrupos.jsx
+++ money-control/src/components/MClistgrupos/mclistgrupos.jsx
@@ -34,6 +34,68 @@ const MClistgrupos = (props) => {
               key={index}
               style={{
                 borderColor: c.color,
+                borderWidth: "5px",
+                borderStyle: "solid",
+                backgroundColor: c.color,
+              }}
+            >
+              <div className="grupo-left" style={{ backgroundColor: c.color }}>
+                <DynamicFaIcon name={c.icono} color="white" />
+              </div>
+              <div className="grupo-right">
+                <div className="grupo-r-l">
+                  <IonLabel className="grupo-name" style={{ color: c.color }}>
+                    {c.name}
+                  </IonLabel>
+                  <div className="gastos_totales">
+                    <span>-1000 €</span>
+                  </div>
+                  {c.codinv ? (
+                    <div className="codinv">CODIGO: {c.codinv}</div>
+                  ) : (
+                    <div className="codinv">Grupo privado</div>
+                  )}
+                </div>
+                <div className="grupo-r-r">
+                  <div className="grupo-options">
+                    <div
+                      className="delete"
+                      onClick={() => {
+                        setSelected(c);
+                        setIsConfirmOpen(true);
+                      }}
+                    >
+                      <DynamicFaIcon
+                        name="clipboardOutline"
+                        color="var(--ion-color-primary)"
+                      />
+                      <DynamicFaIcon
+                        name="exitOutline"
+                        color="var(--ion-color-danger-tint)"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="grupo-r-participantes">
+                  {c.users.length < 2
+                    ? "Solo tú"
+                    : c.users.length + " participantes"}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        "Sin grupos"
+      )}
+      {/* {grupos ? (
+        <div>
+          {grupos.map((c, index) => (
+            <div
+              className="grupo-item"
+              key={index}
+              style={{
+                borderColor: c.color,
                 borderWidth: "4px",
                 borderStyle: "solid",
               }}
@@ -75,7 +137,7 @@ const MClistgrupos = (props) => {
         </div>
       ) : (
         "Sin grupos"
-      )}
+      )} */}
       <IonToast
         isOpen={success.status}
         onDidDismiss={() => setSuccess({ status: false, msg: "" })}
